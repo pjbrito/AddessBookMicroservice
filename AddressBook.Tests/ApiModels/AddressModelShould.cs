b8dd1adbcd20d729a6724b17c3aa4a57e8cfcd9b
Paulo Brito <pjbrito@gmail.com>
2020-01-13 09:43:11 +0000

    added Address model

---
+++ AddressBook.Tests/ApiModels/AddressModelShould.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AddressBook.ApiModels;
+using Xunit;
+
+namespace AddressBook.Tests.ApiModels
+{
+    public class AddressModelShould
+    {
+        [Fact]
+        public void SucceedToCreateInstance()
+        {
+            var u = new Address
+            {
+                FirstName = string.Empty,
+                LastName = string.Empty,
+                StreetAddress = string.Empty,
+                City = string.Empty,
+                Country = string.Empty,
+            };
+
+            Assert.NotNull(u);
+        }
+    }
+}
---
+++ AddressBook/ApiModels/Address.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AddressBook.ApiModels
+{
+    public class Address
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+    }
+}

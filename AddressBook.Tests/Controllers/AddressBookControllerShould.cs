653993eca9336fc19d479230ac17b83ba1965837
Paulo Brito <pjbrito@gmail.com>
2020-01-13 09:25:04 +0000

    added AddressBookController

---
+++ AddressBook.Tests/Controllers/AddressBookControllerShould.cs
@@ -0,0 +1,23 @@
+﻿using AddressBook.Controllers;
+using Xunit;
+
+namespace AddressBook.Tests.Controllers
+{
+    public class AddressBookControllerShould
+    {
+        private readonly AddressBookController controller = null;
+
+        public AddressBookControllerShould()
+        {
+            controller = new AddressBookController();
+        }
+
+        [Fact]
+        public void ReturnData()
+        {
+            var res = controller.GetAddresses();
+
+            Assert.NotEmpty(res);
+        }
+    }
+}
---
+++ AddressBook/Controllers/AddressBookController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AddressBook.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AddressBookController : ControllerBase
+    {
+        // GET: api/AddressBook
+        [HttpGet("GetAllGroupedAddresses")]
+        public IEnumerable<string> GetAddresses()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/AddressBook/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/AddressBook
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/AddressBook/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}

ee9610185ba92ce5629f7785606f6494491178fc
Paulo Brito <pjbrito@gmail.com>
2020-01-13 09:55:19 +0000

    parsing / loading json file

---
+++ AddressBook.Tests/StaticDataFile/FileLoaderShould.cs
@@ -0,0 +1,17 @@
+﻿using AddressBook.StaticDataFile;
+using Xunit;
+
+namespace AddressBook.Tests.StaticDataFile
+{
+    public class FileLoaderShould
+    {
+        [Fact]
+        public void LoadStaticDataFile()
+        {
+            var l = new AddressesStaticDataFileLoader();
+            var data = l.LoadFile();
+
+            Assert.NotEmpty(data);
+        }
+    }
+}
--- AddressBook/StaticDataFile/AddressesStaticDataFileLoader.cs
+++ AddressBook/StaticDataFile/AddressesStaticDataFileLoader.cs
@@ -1,11 +1,35 @@
-﻿using System;
+﻿using AddressBook.ApiModels;
+using Microsoft.Extensions.FileProviders;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
+using System.Text.Json;
 
 namespace AddressBook.StaticDataFile
 {
     public class AddressesStaticDataFileLoader
     {
+        public IEnumerable<Address> LoadFile()
+        {
+            var provider = new PhysicalFileProvider(Environment.CurrentDirectory);
+            var fileI = provider.GetFileInfo(@"StaticDataFile\StaticRepo.json");
+
+            var options = new JsonSerializerOptions
+            {
+                WriteIndented = true,
+                PropertyNameCaseInsensitive = true
+            };
+
+            if (!fileI.Exists)
+            {
+                return new List<Address>();
+            }
+
+            using var sr = new StreamReader(fileI.PhysicalPath);
+            var jsonFile = sr.ReadToEnd();
+            var result = JsonSerializer.Deserialize(jsonFile, typeof(List<Address>), options) as List<Address>;
+
+            return result;
+        }
     }
 }

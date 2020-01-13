bdd3a4e8dd0c805a560c107a2b0cfd93623702e5
Paulo Brito <pjbrito@gmail.com>
2020-01-13 08:58:42 +0000

    added test project

---
+++ AddressBook.Tests/TestEnvironmentShould.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace AddressBook.Tests
+{
+    public class TestEnvironmentShould
+    {
+        [Fact]
+        public void Work()
+        {
+            //Assert.True(false);
+            Assert.True(true);
+        }
+    }
+}

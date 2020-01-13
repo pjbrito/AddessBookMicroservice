9182444ccd13083a26ed2e44d74da50755cd8c38
Paulo Brito <pjbrito@gmail.com>
2020-01-13 10:29:48 +0000

    refactor / cleanup

--- AddressBook/Controllers/AddressBookController.cs
+++ AddressBook/Controllers/AddressBookController.cs
@@ -22,30 +22,5 @@ namespace AddressBook.Controllers
             return new string[] { "value1", "value2" };
         }
 
-        // GET: api/AddressBook/5
-        [HttpGet("{id}", Name = "Get")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST: api/AddressBook
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
-        }
-
-        // PUT: api/AddressBook/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
-        {
-        }
-
-        // DELETE: api/ApiWithActions/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
-
 }

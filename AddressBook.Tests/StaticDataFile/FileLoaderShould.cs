8fd4616350047d76728a1e07c3ec56246e02765d
Paulo Brito <pjbrito@gmail.com>
2020-01-13 09:59:27 +0000

    added more loading tests

--- AddressBook.Tests/StaticDataFile/FileLoaderShould.cs
+++ AddressBook.Tests/StaticDataFile/FileLoaderShould.cs
@@ -1,17 +1,45 @@
 ﻿using AddressBook.StaticDataFile;
+using System.Linq;
 using Xunit;
 
 namespace AddressBook.Tests.StaticDataFile
 {
     public class FileLoaderShould
     {
+        private readonly AddressesStaticDataFileLoader loader = null;
+
+        public FileLoaderShould()
+        {
+            loader = new AddressesStaticDataFileLoader();
+        }
+
         [Fact]
         public void LoadStaticDataFile()
         {
-            var l = new AddressesStaticDataFileLoader();
-            var data = l.LoadFile();
+            var data = loader.LoadFile();
 
             Assert.NotEmpty(data);
         }
+
+        [Fact]
+        public void LoadFirstElementCorrectly()
+        {
+            var data = loader.LoadFile().ToList();
+
+            Assert.Equal("John", data[0].FirstName);
+            Assert.Equal("Smith", data[0].LastName);
+            Assert.Equal("Test St 1", data[0].StreetAddress);
+            Assert.Equal("London", data[0].City);
+            Assert.Equal("England", data[0].Country);
+        }
+
+        [Fact]
+        public void LoadAddresses()
+        {
+            var data = loader.LoadFile();
+
+            Assert.Equal(6, data.Count());
+        }
+
     }
 }
